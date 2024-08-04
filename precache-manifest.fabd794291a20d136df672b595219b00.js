2fb2cb34a9184373642696ed3e6140ade57e0aec
Shillpa HM <shillpahm@Ullas-MacBook-Air.local>
2024-08-04 23:40:26 +0530

    Updates

---
+++ precache-manifest.fabd794291a20d136df672b595219b00.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd2cab785a293cc91df045f10bcadfa",
+    "url": "/modern_restuarant/index.html"
+  },
+  {
+    "revision": "fb969344ecd45d8b53a3",
+    "url": "/modern_restuarant/static/css/main.977a4bd4.chunk.css"
+  },
+  {
+    "revision": "a143bafa074471064c43",
+    "url": "/modern_restuarant/static/js/2.bd9dfd6d.chunk.js"
+  },
+  {
+    "revision": "fb969344ecd45d8b53a3",
+    "url": "/modern_restuarant/static/js/main.d20109c0.chunk.js"
+  },
+  {
+    "revision": "e2ad60d4321fe09e2d05",
+    "url": "/modern_restuarant/static/js/runtime~main.515e598f.js"
+  },
+  {
+    "revision": "c7fdecee851896549a8f719d1d792e75",
+    "url": "/modern_restuarant/static/media/G.c7fdecee.png"
+  },
+  {
+    "revision": "a807ceab88095e0cbece581d1600f52d",
+    "url": "/modern_restuarant/static/media/award01.a807ceab.png"
+  },
+  {
+    "revision": "ff49ca57a8d44a4596c62d4966eb5788",
+    "url": "/modern_restuarant/static/media/award02.ff49ca57.png"
+  },
+  {
+    "revision": "73a36a9eac74bab796a0e1c799a7ad4a",
+    "url": "/modern_restuarant/static/media/award03.73a36a9e.png"
+  },
+  {
+    "revision": "d13582bfb80458cf3abd2268d5b5b581",
+    "url": "/modern_restuarant/static/media/award05.d13582bf.png"
+  },
+  {
+    "revision": "d719065bb2726afe77e90cec8ea81d5a",
+    "url": "/modern_restuarant/static/media/bg.d719065b.png"
+  },
+  {
+    "revision": "85d27f24ca6480fa5e10c95311f10e19",
+    "url": "/modern_restuarant/static/media/chef.85d27f24.png"
+  },
+  {
+    "revision": "03ef4140ada413449ce1d79046217392",
+    "url": "/modern_restuarant/static/media/findus.03ef4140.png"
+  },
+  {
+    "revision": "5a3891e8d95239f82c09715ae11941b6",
+    "url": "/modern_restuarant/static/media/gallery01.5a3891e8.png"
+  },
+  {
+    "revision": "2dd1c3366e4ba309e1ab5ef1c4d31f2d",
+    "url": "/modern_restuarant/static/media/gallery02.2dd1c336.png"
+  },
+  {
+    "revision": "3706b6f7f3577e20cbc4bec6e7698eb5",
+    "url": "/modern_restuarant/static/media/gallery03.3706b6f7.png"
+  },
+  {
+    "revision": "f51d9759a0055fffba51cb9949949771",
+    "url": "/modern_restuarant/static/media/gallery04.f51d9759.png"
+  },
+  {
+    "revision": "e96f9bd45b8e0b34eeb03e93f547ccf8",
+    "url": "/modern_restuarant/static/media/knife.e96f9bd4.png"
+  },
+  {
+    "revision": "c2d7d58a629c456aac6406b8986272fb",
+    "url": "/modern_restuarant/static/media/laurels.c2d7d58a.png"
+  },
+  {
+    "revision": "5c00c519489e1238ee18cc42a47f141a",
+    "url": "/modern_restuarant/static/media/logo.5c00c519.png"
+  },
+  {
+    "revision": "be2151f4e9af529f67ce80896ed8374a",
+    "url": "/modern_restuarant/static/media/meal.be2151f4.mp4"
+  },
+  {
+    "revision": "cc54616224f2b073f9400318c172d8a6",
+    "url": "/modern_restuarant/static/media/menu.cc546162.png"
+  },
+  {
+    "revision": "77d1950a460b03e7ba7c6b08ea240f4a",
+    "url": "/modern_restuarant/static/media/meridian.77d1950a.png"
+  },
+  {
+    "revision": "179769474d7506d29c78a6bec834b00c",
+    "url": "/modern_restuarant/static/media/overlaybg.17976947.png"
+  },
+  {
+    "revision": "f1363e1deeb3ee759b3a365913a97c85",
+    "url": "/modern_restuarant/static/media/sign.f1363e1d.png"
+  },
+  {
+    "revision": "72422a1acc4f48e3d82ae3bfa6d1b9d8",
+    "url": "/modern_restuarant/static/media/spoon.72422a1a.svg"
+  },
+  {
+    "revision": "fd16528f71a845cc74618cf2bd3d8995",
+    "url": "/modern_restuarant/static/media/welcome.fd16528f.png"
+  }
+]);
